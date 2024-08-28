27c4a31ab7fe8fc4519bc2a8dde10ee8e8cd02ad
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-28 13:11:11 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Very_bg_dark_blue: "hsl(233, 47%, 7%)",
+                c_Dark_card_bg_desaturated_blue: "hsl(244, 38%, 16%)",
+                c_Soft_accent_violet: "hsl(277, 64%, 61%)",
+                c_Slightly_main_paragraph_transparent_white : "hsla(0, 0%, 100%, 0.75)",
+                c_Slightly_stat_headings_transparent_white : "hsla(0, 0%, 100%, 0.6)",
             },
             fontFamily: {
-
+                inter:[ "Inter", "sans-serif"],
+                lexend_deca: ["Lexend Deca", "sans-serif"]
             }
         },
     },
